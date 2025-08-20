6a3bc6677a76b9b4fc4395d5f3c5e337503b02d2
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2025-08-21 02:59:06 +0530

    Update variables.py

--- colab_leecher/utility/variables.py
+++ colab_leecher/utility/variables.py
@@ -12,9 +12,8 @@ class BOT:
     class Setting:
         stream_upload = "Media"
         convert_video = "Yes"
-        convert_quality = "Low"
+        convert_quality = "High"
         caption = "Monospace"
-        split_video = "Split Videos"
         prefix = ""
         suffix = ""
         thumbnail = False
@@ -22,8 +21,7 @@ class BOT:
     class Options:
         stream_upload = True
         convert_video = True
-        convert_quality = False
-        is_split = True
+        convert_quality = True
         caption = "code"
         video_out = "mp4"
         custom_name = ""
@@ -100,7 +98,6 @@ class Messages:
 class MSG:
     sent_msg = Message(id=1)
     status_msg = Message(id=2)
-
 
 
 class Aria2c:
