33ab00b6d27ba17adc09b3c557f37c783a2f8978
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2025-10-29 23:50:01 +0530

    Update __init__.py

--- colab_leecher/__init__.py
+++ colab_leecher/__init__.py
@@ -1,8 +1,12 @@
-# copyright 2023 © Xron Trix | https://github.com/Xrontrix10
+import asyncio
+import logging
+import json
+import nest_asyncio
 
-import asyncio, logging, json
-# from uvloop import install
 from pyrogram.client import Client
+
+# 2. Pyrogram පාවිච්චි කරන්න කලින් මේ line එක දාන්න
+nest_asyncio.apply()
 
 # Read the dictionary from the txt file
 with open("/content/Telegram-Leecher/credentials.json", "r") as file:
@@ -14,16 +18,8 @@ BOT_TOKEN = credentials["BOT_TOKEN"]
 OWNER = credentials["USER_ID"]
 DUMP_ID = credentials["DUMP_ID"]
 
-
 logging.basicConfig(level=logging.INFO)
 
 
-#install()
-
-# ensure event loop exists before using Pyrogram
-try:
-    asyncio.get_event_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
+# දැන් Client එක හදන්න
 colab_bot = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
